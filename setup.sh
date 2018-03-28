68cc1f08e3dc706e791c474778d7891471db59f9
inoue-shinta <s_inoue@r.recruit.co.jp>
2018-03-28 15:49:01 +0900

    Add vim settings

--- setup.sh
+++ setup.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -eu
 
-DOT_FILES=(.zshrc .atom)
+DOT_FILES=(.zshrc .atom .vimrc)
 echo "Set up dotfiles -> ${DOT_FILES[@]}"
-
-backupDir=$HOME/dotfiles.bk
-if [ -e $backupDir ]; then
-  rm -rf $backupDir
-fi
-mkdir $backupDir
 
 for file in ${DOT_FILES[@]}
 do
-  actualFile=$HOME/$file
-  if [ ! -L $actualFile ]; then
-    if [ -e $actualFile ]; then
-      echo "move ${actualFile} to backup dir"
-      mv $actualFile $backupDir/$file
-    fi
-    ln -s $HOME/dotfiles/$file $HOME/$file
-  fi
+  ln -sf $HOME/dotfiles/$file $HOME
+done
+
+VIM_CONFIG=(colors)
+echo "Set up vim configs -> ${VIM_CONFIG[@]}"
+
+for dir in ${VIM_CONFIG[@]}
+do
+  ln -sf $HOME/dotfiles/$dir $HOME/.vim
 done
 
 echo "Complete!"
